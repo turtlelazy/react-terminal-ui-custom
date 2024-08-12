a9686daee1c567ec3aa26a599a6900d6ae048ff0
turtlelazy <ishraqmahid@gmail.com>
2024-08-12 02:26:46 -0400

    adding randomg space

--- src/index.tsx
+++ src/index.tsx
@@ -128,7 +128,7 @@ const Terminal = ({ name, sshDir, fileDir, twindClass, height = "600px", colorMo
         { children }
         { typeof onInput === 'function' && 
           <TerminalInput sshDir={sshDir} fileDir={fileDir} twindClass={twindClass} activeCursor={true} >
-                      { currentLineInput } <span className="cursor" style={{ left: `${cursorPos + 3}px` }}></span>
+                      { currentLineInput } <span className="cursor" style={{ left: `${cursorPos + 1}px` }}></span>
           </TerminalInput>
         }
         <div ref={ scrollIntoViewRef }></div>
